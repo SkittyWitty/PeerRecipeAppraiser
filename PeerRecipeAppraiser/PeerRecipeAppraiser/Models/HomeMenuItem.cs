1e2cfc0b07cc91c56b3036c086a560cd2f4d98e5
Mindy DeWaal <kawiititan@gmail.com>
2020-05-19 20:29:42 -0700

    Initial commit

---
+++ PeerRecipeAppraiser/PeerRecipeAppraiser/Models/HomeMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeerRecipeAppraiser.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ PeerRecipeAppraiser/PeerRecipeAppraiser/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PeerRecipeAppraiser.Models;
+
+namespace PeerRecipeAppraiser.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
